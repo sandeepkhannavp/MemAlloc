0827393c760ac77b17ba354ab74b433664bd7ec8
sandeepkhannavp <vpsandeepkhanna@gmail.com>
2024-06-08 16:49:19 +0530

    page family registration and moved testing components as seperate file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # MemAlloc
 
 Re-writing the OS Coursework project - Linux Memory manager
+
+for running,
+
+gcc -g -c mm.c -o mm.o
+gcc -g -c testapp.c -o testapp.o
+gcc -g mm.o testapp.o -o exe
--- mm.c
+++ mm.c
@@ -10,6 +10,7 @@
 
 
 static size_t SYSTEM_PAGE_SIZE = 0;
+static vm_page_for_families_t *first_vm_page_for_families = NULL; //head
 
 void *gb_hsba = NULL;
 
@@ -43,21 +44,51 @@ static void mm_return_vm_page_to_kernel(void *ptr, int units){
     }
 }
 
-int main(){
-    mm_init();
-    printf("VM page size is = %lu\n", SYSTEM_PAGE_SIZE);
 
-    void *addr1 = mm_get_new_vm_page_from_kernel(1);
-    void *addr2 = mm_get_new_vm_page_from_kernel(1);
+//user space application calls this function (API) to register page family information
+void
+mm_instantiate_new_page_family(
+    char *struct_name,
+    uint32_t struct_size){
 
-    printf("page1 = %p, page2 = %p\n", addr1, addr2);
+    vm_page_family_t *vm_page_family_curr = NULL;
+    vm_page_for_families_t *new_vm_page_for_families = NULL;
+    vm_page_for_families_t *vm_page_for_families_global;
 
-    if (addr1) {
-        mm_return_vm_page_to_kernel(addr1, 1);
+    if(struct_size>SYSTEM_PAGE_SIZE){
+        printf("Error: %s() structure %s size exceeds system page size\n", __FUNCTION__, struct_name);
+        return;
     }
-    if (addr2) {
-        mm_return_vm_page_to_kernel(addr2, 1);
+
+    if(!first_vm_page_for_families){
+        first_vm_page_for_families = (vm_page_for_families_t *)mm_get_new_vm_page_from_kernel(1);
+        first_vm_page_for_families->next = NULL;
+        strncpy(vm_page_for_families_global->vm_page_family[0].struct_name, struct_name,
+            MM_MAX_STRUCT_NAME);
+        first_vm_page_for_families->vm_page_family[0].struct_size = struct_size;
+        return;
+    }
+    uint32_t count = 0;
+    
+    ITERATE_PAGE_FAMILIES_BEGIN(first_vm_page_for_families, vm_page_family_curr){
+        if(strncmp(vm_page_family_curr->struct_name,struct_name,MM_MAX_STRUCT_NAME)!=0){
+            count++;
+            continue;
+        }
+        assert(0);
+
+    } ITERATE_PAGE_FAMILIES_END(first_vm_page_for_families, vm_page_family_curr);
+
+    if(count == MAX_FAMILIES_PER_VM_PAGE){
+        /*Request a new vm page from kernel to add a new family*/
+        new_vm_page_for_families = (vm_page_for_families_t *)mm_get_new_vm_page_from_kernel(1);
+        new_vm_page_for_families->next = first_vm_page_for_families;
+        first_vm_page_for_families = new_vm_page_for_families;
+        vm_page_family_curr = &first_vm_page_for_families->vm_page_family[0];
     }
 
-    return 0;
+    strncpy(vm_page_family_curr->struct_name, struct_name,
+            MM_MAX_STRUCT_NAME);
+    vm_page_family_curr->struct_size = struct_size;
 }
+
