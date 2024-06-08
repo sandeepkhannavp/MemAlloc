80ebbe79e9095533d908ca5a959aa4fff147c161
sandeepkhannavp <vpsandeepkhanna@gmail.com>
2024-06-08 21:35:14 +0530

    Meta and data blocks and associated macros added

--- mm.h
+++ mm.h
@@ -1,34 +1,68 @@
-#ifndef __MM_
+#ifndef __MM__
 #define __MM__
 
+#include <stdint.h> /*uint32_t*/
+
 #define MM_MAX_STRUCT_NAME 32
+
+typedef enum{
+
+    MM_FALSE,
+    MM_TRUE
+} vm_bool_t;
+
+
+struct vm_page_family_;
 typedef struct vm_page_family_{
 
     char struct_name[MM_MAX_STRUCT_NAME];
     uint32_t struct_size;
 } vm_page_family_t;
 
-//Linked list structure to store page family information
 typedef struct vm_page_for_families_{
 
     struct vm_page_for_families_ *next;
     vm_page_family_t vm_page_family[0];
 } vm_page_for_families_t;
 
-//macro which specifies number of page family information per VM page
-#define MAX_FAMILIES_PER_VM_PAGE    \
-    (SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *)/\
-        sizeof(vm_page_family_t))
+#define MAX_FAMILIES_PER_VM_PAGE   \
+    ((SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *))/sizeof(vm_page_family_t))
 
-//iterate the page families inside a VM page 
+typedef struct block_meta_data_{
+
+    vm_bool_t is_free;
+    uint32_t block_size;
+    uint32_t offset;    //offset from the start of the page
+    struct block_meta_data_ *prev_block;
+    struct block_meta_data_ *next_block;
+} block_meta_data_t;
+
+#define offset_of(container_structure, field_name)  \
+    ((size_t)&(((container_structure *)0)->field_name))
+
+#define MM_GET_PAGE_FROM_META_BLOCK(block_meta_data_ptr)    \
+    ((void * )((char *)block_meta_data_ptr - block_meta_data_ptr->offset))
+
+#define NEXT_META_BLOCK(block_meta_data_ptr)    \
+    (block_meta_data_ptr->next_block)
+
+#define NEXT_META_BLOCK_BY_SIZE(block_meta_data_ptr)    \
+    (block_meta_data_t *)((char *)(block_meta_data_ptr + 1) \
+        + block_meta_data_ptr->block_size)
+
+#define PREV_META_BLOCK(block_meta_data_ptr)    \
+    (block_meta_data_ptr->prev_block)
+
 #define ITERATE_PAGE_FAMILIES_BEGIN(vm_page_for_families_ptr, curr)       \
-{                                                                         \
-    uint32_t count = 0;                                                   \
+{                                                            \
+    uint32_t count = 0;                                      \
     for(curr = (vm_page_family_t *)&vm_page_for_families_ptr->vm_page_family[0];   \
-        curr->struct_size && count < MAX_FAMILIES_PER_VM_PAGE;             \
+        curr->struct_size && count < MAX_FAMILIES_PER_VM_PAGE; \
         curr++,count++){
 
 #define ITERATE_PAGE_FAMILIES_END(vm_page_for_families_ptr, curr)   }}
 
+vm_page_family_t *
+lookup_page_family_by_name(char *struct_name);
 
-#endif 
+#endif /**/
