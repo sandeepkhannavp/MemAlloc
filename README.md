6e4de4adc27b604caa9227521dbe10fc1a774a01
sandeepkhannavp <vpsandeepkhanna@gmail.com>
2024-06-09 00:14:36 +0530

    VM page insertion and deletion utility functions added

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Re-writing the OS Coursework project - Linux Memory manager
 
-for running,
+for running the memoru manager,
 
-gcc -g -c mm.c -o mm.o
 gcc -g -c testapp.c -o testapp.o
-gcc -g mm.o testapp.o -o exe
+gcc -g -c mm.c -o mm.o
+gcc -g -c gluethread/glthread.c -o gluethread/glthread.o
+gcc -g gluethread/glthread.o mm.o testapp.o -o test.exe
--- mm.h
+++ mm.h
@@ -9,13 +9,56 @@ typedef enum{
     MM_TRUE
 } vm_bool_t;
 
-/*Forward Declaration*/
+typedef struct block_meta_data_{
+
+    vm_bool_t is_free;
+    uint32_t block_size;
+    uint32_t offset;  
+    struct block_meta_data_ *prev_block;
+    struct block_meta_data_ *next_block;
+} block_meta_data_t;
+
+#define offset_of(container_structure, field_name)  \
+    ((size_t)&(((container_structure *)0)->field_name))
+
 struct vm_page_family_;
+
+typedef struct vm_page_{
+    struct vm_page_ *next;
+    struct vm_page_ *prev;
+    struct vm_page_family_ *pg_family; 
+    block_meta_data_t block_meta_data;
+    char page_memory[0];
+} vm_page_t;
+
+#define MM_GET_PAGE_FROM_META_BLOCK(block_meta_data_ptr)    \
+    ((void * )((char *)block_meta_data_ptr - block_meta_data_ptr->offset))
+
+#define NEXT_META_BLOCK(block_meta_data_ptr)                \
+    (block_meta_data_ptr->next_block)
+
+#define NEXT_META_BLOCK_BY_SIZE(block_meta_data_ptr)        \
+    (block_meta_data_t *)((char *)(block_meta_data_ptr + 1) \
+        + block_meta_data_ptr->block_size)
+
+#define PREV_META_BLOCK(block_meta_data_ptr)    \
+    (block_meta_data_ptr->prev_block)
+
+#define mm_bind_blocks_for_allocation(allocated_meta_block, free_meta_block)  \
+    free_meta_block->prev_block = allocated_meta_block;        \
+    free_meta_block->next_block = allocated_meta_block->next_block;    \
+    allocated_meta_block->next_block = free_meta_block;                \
+    if (free_meta_block->next_block)                                   \
+    free_meta_block->next_block->prev_block = free_meta_block
+
+vm_bool_t mm_is_vm_page_empty(vm_page_t *vm_page);
+
 #define MM_MAX_STRUCT_NAME 32
 typedef struct vm_page_family_{
 
     char struct_name[MM_MAX_STRUCT_NAME];
     uint32_t struct_size;
+    vm_page_t *first_page;
 } vm_page_family_t;
 
 typedef struct vm_page_for_families_{
@@ -27,49 +70,44 @@ typedef struct vm_page_for_families_{
 #define MAX_FAMILIES_PER_VM_PAGE   \
     ((SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *))/sizeof(vm_page_family_t))
 
-typedef struct block_meta_data_{
+vm_page_t *allocate_vm_page();
 
-    vm_bool_t is_free;
-    uint32_t block_size;
-    uint32_t offset;    /*offset from the start of the page*/
-    struct block_meta_data_ *prev_block;
-    struct block_meta_data_ *next_block;
-} block_meta_data_t;
+#define MARK_VM_PAGE_EMPTY(vm_page_t_ptr)                                 \
+    vm_page_t_ptr->block_meta_data.next_block = NULL;                     \
+vm_page_t_ptr->block_meta_data.prev_block = NULL;                         \
+vm_page_t_ptr->block_meta_data.is_free = MM_TRUE
 
-#define offset_of(container_structure, field_name)  \
-    ((size_t)&(((container_structure *)0)->field_name))
+#define ITERATE_VM_PAGE_BEGIN(vm_page_family_ptr, curr)   \
+{                                             \
+    curr = vm_page_family_ptr->first_page;    \
+    vm_page_t *next = NULL;                   \
+    for(; curr; curr = next){                 \
+        next = curr->next;
 
-#define MM_GET_PAGE_FROM_META_BLOCK(block_meta_data_ptr)    \
-    ((void * )((char *)block_meta_data_ptr - block_meta_data_ptr->offset))
+#define ITERATE_VM_PAGE_END(vm_page_family_ptr, curr)   \
+    }}
 
-#define NEXT_META_BLOCK(block_meta_data_ptr)    \
-    (block_meta_data_ptr->next_block)
+#define ITERATE_VM_PAGE_ALL_BLOCKS_BEGIN(vm_page_ptr, curr)    \
+{                                                              \
+    curr = &vm_page_ptr->block_meta_data;                      \
+    block_meta_data_t *next = NULL;                            \
+    for( ; curr; curr = next){                                 \
+        next = NEXT_META_BLOCK(curr);
 
-#define NEXT_META_BLOCK_BY_SIZE(block_meta_data_ptr)    \
-    (block_meta_data_t *)((char *)(block_meta_data_ptr + 1) \
-        + block_meta_data_ptr->block_size)
+#define ITERATE_VM_PAGE_ALL_BLOCKS_END(vm_page_ptr, curr)      \
+    }}
 
-#define PREV_META_BLOCK(block_meta_data_ptr)    \
-    (block_meta_data_ptr->prev_block)
-
-#define ITERATE_PAGE_FAMILIES_BEGIN(vm_page_for_families_ptr, curr)       \
-{                                                            \
-    uint32_t count = 0;                                      \
-    for(curr = (vm_page_family_t *)&vm_page_for_families_ptr->vm_page_family[0];   \
-        curr->struct_size && count < MAX_FAMILIES_PER_VM_PAGE; \
+#define ITERATE_PAGE_FAMILIES_BEGIN(vm_page_for_families_ptr, curr)                 \
+{                                                                                   \
+    uint32_t count = 0;                                                             \
+    for(curr = (vm_page_family_t *)&vm_page_for_families_ptr->vm_page_family[0];    \
+        curr->struct_size && count < MAX_FAMILIES_PER_VM_PAGE;                      \
         curr++,count++){
 
 #define ITERATE_PAGE_FAMILIES_END(vm_page_for_families_ptr, curr)   }}
 
-#define mm_bind_blocks_for_allocation(allocated_meta_block, free_meta_block)  \
-    free_meta_block->prev_block = allocated_meta_block;        \
-    free_meta_block->next_block = allocated_meta_block->next_block;    \
-    allocated_meta_block->next_block = free_meta_block;                \
-    if (free_meta_block->next_block)\
-    free_meta_block->next_block->prev_block = free_meta_block
-
-
 vm_page_family_t *
 lookup_page_family_by_name(char *struct_name);
 
+void mm_vm_page_delete_and_free(vm_page_t *vm_page);
 #endif /**/
